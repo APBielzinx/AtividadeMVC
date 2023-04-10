e30b6f4731a196c4d81dd31c65680f836355fe9a
mariaresolino <maria.resolino1001@gmail.com>
2023-04-10 20:16:10 -0300

    criando arquivos triangulo

---
+++ triangulo/model/Usuario.php
@@ -0,0 +1,32 @@
+<?php
+
+class Usuario {
+    private ladoA;
+    private ladoB;
+    private ladoC;
+
+
+    public function getLadoA() {
+        return $this->ladoA;
+    }
+    public function setLadoA($ladoA) {
+        $this->ladoA = $ladoA;
+    }
+
+    public function getLadoB() {
+        return $this->ladoA;
+    }
+    public function setLadoB($ladoB) {
+        $this->ladoB = $ladoB;
+    }
+
+    public function getLadoC() {
+        return $this->ladoC;
+    }
+    public function setLadoC($ladoC) {
+        $this->ladoC = $ladoC;
+    }
+
+}
+
+?>
